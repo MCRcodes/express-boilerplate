0ac0bd302ffd650bc3a319a9e921cd5668575e43
Joe Stephens <joe@webantic.co.uk>
2018-06-18 20:47:38 +0100

    add mongoose

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import express from 'express';
 import chalk from 'chalk';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import path from 'path';
+import mongoose from 'mongoose';
+
+require('dotenv').config({
+  path: path.join(__dirname, './.env'),
+});
+
+mongoose.connect(process.env.DATABASE_CONN);
 
 const app = express();
 
